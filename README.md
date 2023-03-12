0765cf647f77cd2350ac443a3f1255a6bc8f3d42
nikhil123nikhil <106946553+nikhil123nikhil@users.noreply.github.com>
2023-03-12 17:59:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Tryitrightnow
-Tryitrightnlw
+# m3uSave Bot
+A Telegram bot to Download and Convert m3u Links to VIDEO
+---
+
+## Variables
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+- `IPTV_LINK` The URL which contains data for IPTV
+
+---
---
+++ config.py
@@ -0,0 +1,23 @@
+sudo_users = [1984763765]
+
+iptv_link = "https://gist.githubusercontent.com/adityaraj2003/c8052a548e3bce3f2ef69f8ac5a593f4/raw/050ccc515b1f6d9d00502cfe132eecfd1a589168/iptv.json"
+
+GROUP_TAG = "Conan76"
+
+
+CHANNELS_TEXT = """<b>Here are the List of Channels : </b>
+
+<code>{}</code>
+
+<b>Usage : </b> <code>/multirec channelTitle duration | showTitle</code> 
+
+<b>Example : </b> <code>/multirec Disney 00:00:10 | Doraemon</code> 
+"""
+
+
+DL_DONE_MSG = """
+✅ <b> {} Completed In </b> <code>{}</code>
+<b>FileName : </b> <code>{}</code>
+<b>Channel : </b> <code>{}</code>
+<b>Size : </b> <code>{}</code>
+"""
